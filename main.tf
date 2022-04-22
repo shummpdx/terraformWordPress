ac0c20748ff4267299ab1135efa4438eb69422e4
Sean Humm <hummsean@pdx.edu>
2022-04-22 15:00:16 -0700

    formatting fixes

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
-provider "aws" {
-  region = var.region
-}
-
 # Assign the private key that was created on my local computer
 data "tls_public_key" "example" {
   private_key_pem = "${file(var.PRIVATE_KEY_PATH)}"
 }
 
-
-# Deploy(?) the key pay
+# Deploy the key pay
 resource "aws_key_pair" "deployer" {
   key_name = "ec2Key"
   public_key = "${file(var.PUBLIC_KEY_PATH)}" 
@@ -16,7 +11,7 @@ resource "aws_key_pair" "deployer" {
 
 # Build our Configured EC2 Instance
 resource "aws_instance" "Wordpress" {
-    ami =  var.instance_ami #Ubuntu, 20.04 LTS
+    ami = var.instance_ami #Ubuntu, 20.04 LTS
     instance_type = var.instance_type 
     subnet_id = aws_subnet.wordpress_public_a.id
     security_groups = ["${aws_security_group.wordpress_security.id}"]
