9bd7bb57b5f6533337faae9676a16645ae240526
Sean Humm <hummsean@pdx.edu>
2022-06-16 17:23:41 -0700

    remove internet gateway

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,47 @@
+# Create new VPC
 resource "aws_vpc" "production" {
   cidr_block = var.cidr_block
   instance_tenancy = "default"
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "Production"
+    Name = "Production Test"
   }
 }
 
+resource "aws_internet_gateway" "defaultIG" {
+  vpc_id = aws_vpc.production.id
+  tags = {
+    Name = "Practice IG"
+  }
+}
+
+resource "aws_route_table" "productionRoutes" {
+  vpc_id = aws_vpc.production.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.defaultIG.id
+  }
+
+  tags = {
+    Name = "Practice Route Table"
+  }
+}
+
+resource "aws_route_table_association" "associateMe" {
+  subnet_id = aws_subnet.wordpress_public_a.id
+  route_table_id = aws_route_table.productionRoutes.id
+}
+
 # Create private subnets for the RDS subnet group
+# Two private subnets are requried for db subnet group
 resource "aws_subnet" "rds_private_a" {
   vpc_id = aws_vpc.production.id
   cidr_block = var.subnet1_cidr 
   availability_zone = var.west2a_AZ
   tags = {
-    Name = "RDS Private A"
+    Name = "RDS Private A Test"
   }
 }
 
@@ -22,7 +50,7 @@ resource "aws_subnet" "rds_private_b" {
   cidr_block = var.subnet2_cidr 
   availability_zone = var.west2b_AZ
   tags = {
-    Name = "RDS Private B"
+    Name = "RDS Private B Test"
   }
 }
 
@@ -31,16 +59,8 @@ resource "aws_db_subnet_group" "privateSubnets" {
   subnet_ids = [aws_subnet.rds_private_a.id, aws_subnet.rds_private_b.id]
 
   tags = {
-    Name = "Private Subnet Group"
+    Name = "Private Subnet Group Test"
   }
-}
-
-resource "aws_eip" "example" {
-}
-
-resource "aws_nat_gateway" "PrivateNat" {
-  subnet_id = aws_subnet.rds_private_a.id
-  allocation_id = aws_eip.example.id
 }
 
 resource "aws_subnet" "wordpress_public_a" {
@@ -49,6 +69,6 @@ resource "aws_subnet" "wordpress_public_a" {
   availability_zone = var.west2c_AZ 
   map_public_ip_on_launch = true
   tags = {
-    Name = "WordPress Public A"
+    Name = "WordPress Public A Test"
   }
 }
