0eb729cd794084f05fbf077a6eaf52fbf8d45a78
shummpdx <sean.humm@gmail.com>
2024-01-17 13:44:35 -0800

    skip_final_snapshot = true

--- rds.tf
+++ rds.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "wordPress" {
     db_name = "wordpress"
     username = "admin"
     password = "password"
-    skip_final_snapshot = false
+    skip_final_snapshot = true 
     db_subnet_group_name = aws_db_subnet_group.privateSubnets.id 
     vpc_security_group_ids = [aws_security_group.rds_security.id]
 
